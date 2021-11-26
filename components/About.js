b167e24b1d38402de13a070eb1c56965c0e572de
Ralph <ralphestor30@gmail.com>
2021-11-27 07:51:24 +0800

    removed about divs

--- components/About.js
+++ components/About.js
@@ -3,25 +3,25 @@ import SectionTitle from './SectionTitle';
 
 const About = () => {
     return ( 
-        <section>
+        <main>
             <SectionTitle name="About Me" id="about"/>
             <div className="aboutGrid">
                 <div className="aboutGrid1">
-                    <div className="wrapperDiv">
+                    {/* <div className="wrapperDiv">
                     <Image
                         src="/RalphAbout.png"
                         alt="Ralph Estor"
                         width={300}
                         height={300}
                     />
-                    </div>
+                    </div> */}
                 </div>
                 <div className="aboutGrid2">
-                    <h4 className="aboutLbl">Overview</h4>
-                    <p className="paragraph overPar">Hi! I'm Ralph, a 22-year old web developer from Davao City, Philippines. I'm an Electronics Engineeering student who decided to code for a living because I feel the achievement everytime I create software applications (Please hire me hahaha). </p>
+                    {/* <h4 className="aboutLbl">Overview</h4>
+                    <p className="paragraph overPar">Hi! I'm Ralph, a 22-year old web developer from Davao City, Philippines. I'm an Electronics Engineeering student who decided to code for a living because I feel the achievement everytime I create software applications (Please hire me hahaha). </p> */}
                 </div>
             </div>
-        </section>
+        </main>
      );
 }
  
