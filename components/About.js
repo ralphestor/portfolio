ab865a2dbf1a0e10e2e28ee4d8d35c53f725c62f
Ralph <ralphestor30@gmail.com>
2021-11-26 17:57:19 +0800

    removed tech stack and about

--- components/About.js
+++ components/About.js
@@ -1,53 +1,53 @@
-import Image from 'next/image';
-import SectionTitle from './SectionTitle';
+// import Image from 'next/image';
+// import SectionTitle from './SectionTitle';
 
-const About = () => {
-    return ( 
-        <main>
-            <SectionTitle name="About Me" id="about"/>
-            <div className="aboutGrid">
-                <div className="grid1">
-                    <Image
-                        src="/RalphAbout.png"
-                        alt="Ralph Estor"
-                        width={400}
-                        height={400}
-                    />
-                </div>
-                <div className="grid2">
-                    <div>
-                        <h4>Overview</h4>
-                        <p>Hi! I'm Ralph, a 22-year old web developer. I'm an Electronics Engineeering student who decided to code for a living because I feel the achievement everytime I create software applications. (Please hire me hahaha)  </p>
-                    </div>
-                    <div>
-                        <h4>Technical Skills</h4>
-                        <ul>
-                            <li>Web Development</li>
-                            <li>Web Design</li>
-                            <li>Photo Editing</li>
-                            <li>Mathematical Skills</li>
-                        </ul>    
-                        <h4>Professional Traits</h4>                    
-                        <ul>
-                            <li>Critical Thinker and Problem Solver</li>
-                            <li>Well-Organized and has Good Time Management </li>
-                            <li>Keen Eye for Aesthetics</li>
-                            <li>Trainable and Hardworking</li>
-                        </ul>
-                        <h4>Hobbies</h4>                    
-                        <ul>
-                            <li>Sports: Football and Chess</li>
-                            <li>Arts: Singing and Playing the Guitar </li>
-                            <li>Watching K-Drama Series</li>
-                            <li>Cooking and Eating</li>
-                        </ul>
-                    </div>
+// const About = () => {
+//     return ( 
+//         <main>
+//             <SectionTitle name="About Me" id="about"/>
+//             <div className="aboutGrid">
+//                 <div className="grid1">
+//                     <Image
+//                         src="/RalphAbout.png"
+//                         alt="Ralph Estor"
+//                         width={400}
+//                         height={400}
+//                     />
+//                 </div>
+//                 <div className="grid2">
+//                     <div>
+//                         <h4>Overview</h4>
+//                         <p>Hi! I'm Ralph, a 22-year old web developer. I'm an Electronics Engineeering student who decided to code for a living because I feel the achievement everytime I create software applications. (Please hire me hahaha)  </p>
+//                     </div>
+//                     <div>
+//                         <h4>Technical Skills</h4>
+//                         <ul>
+//                             <li>Web Development</li>
+//                             <li>Web Design</li>
+//                             <li>Photo Editing</li>
+//                             <li>Mathematical Skills</li>
+//                         </ul>    
+//                         <h4>Professional Traits</h4>                    
+//                         <ul>
+//                             <li>Critical Thinker and Problem Solver</li>
+//                             <li>Well-Organized and has Good Time Management </li>
+//                             <li>Keen Eye for Aesthetics</li>
+//                             <li>Trainable and Hardworking</li>
+//                         </ul>
+//                         <h4>Hobbies</h4>                    
+//                         <ul>
+//                             <li>Sports: Football and Chess</li>
+//                             <li>Arts: Singing and Playing the Guitar </li>
+//                             <li>Watching K-Drama Series</li>
+//                             <li>Cooking and Eating</li>
+//                         </ul>
+//                     </div>
 
-                </div>
+//                 </div>
 
-            </div>    
-        </main>
-     );
-}
+//             </div>    
+//         </main>
+//      );
+// }
  
-export default About;
+// export default About;
--- components/TechStack.js
+++ components/TechStack.js
@@ -1,24 +1,24 @@
-import { Icon } from '@iconify/react';
+// import { Icon } from '@iconify/react';
 
-const TechStack = () => {
-    return (
-        <section>
-            <h4 className="textStackLbl">Tech Stack I Use for my Projects</h4>
-            <div className="gridIcons">
-                <Icon className="gridIcon" icon="vscode-icons:file-type-html" height="80px"/>
-                <Icon className="gridIcon" icon="vscode-icons:file-type-css" height="80px"/>
-                <Icon className="gridIcon" icon="logos:sass" height="80" />
-                <Icon className="gridIcon" icon="logos:javascript" height="80px"/>
-                <Icon className="gridIcon" icon="logos:jquery" height="80" />
-                <Icon className="gridIcon" icon="logos:mongodb" height="80" />
-                <Icon className="gridIcon" icon="logos:express" height="80" />
-                <Icon className="gridIcon" icon="logos:react" height="80" />
-                <Icon className="gridIcon" icon="logos:material-ui" height="80" />
-                <Icon className="gridIcon" icon="logos:nextjs" height="80" />
-            </div>
-            <p className="paragraph">Strong foundational knowledge of <b>HTML</b>, <b>CSS</b>, and <b>JavaScript</b>. <b>SASS</b> for browser compatible CSS and developer-friendly syntax and features. <b>JQuery</b> for frontend enhancement purposes. <b>Express</b> for REST applications and easy middleware management. <b>MongoDB</b> for NoSQL database connection for web applications. <b>React</b> for code reusability and performance enhancement. <b>Material UI</b> for pre-styled React Components for fast frontend development. <b>Next.js</b> for super fast performance from Server Side Rendering and for improved SEO. </p>
-        </section>
-     );
-}
+// const TechStack = () => {
+//     return (
+//         <section>
+//             <h4 className="textStackLbl">Tech Stack I Use for my Projects</h4>
+//             <div className="gridIcons">
+//                 <Icon className="gridIcon" icon="vscode-icons:file-type-html" height="80px"/>
+//                 <Icon className="gridIcon" icon="vscode-icons:file-type-css" height="80px"/>
+//                 <Icon className="gridIcon" icon="logos:sass" height="80" />
+//                 <Icon className="gridIcon" icon="logos:javascript" height="80px"/>
+//                 <Icon className="gridIcon" icon="logos:jquery" height="80" />
+//                 <Icon className="gridIcon" icon="logos:mongodb" height="80" />
+//                 <Icon className="gridIcon" icon="logos:express" height="80" />
+//                 <Icon className="gridIcon" icon="logos:react" height="80" />
+//                 <Icon className="gridIcon" icon="logos:material-ui" height="80" />
+//                 <Icon className="gridIcon" icon="logos:nextjs" height="80" />
+//             </div>
+//             <p className="paragraph">Strong foundational knowledge of <b>HTML</b>, <b>CSS</b>, and <b>JavaScript</b>. <b>SASS</b> for browser compatible CSS and developer-friendly syntax and features. <b>JQuery</b> for frontend enhancement purposes. <b>Express</b> for REST applications and easy middleware management. <b>MongoDB</b> for NoSQL database connection for web applications. <b>React</b> for code reusability and performance enhancement. <b>Material UI</b> for pre-styled React Components for fast frontend development. <b>Next.js</b> for super fast performance from Server Side Rendering and for improved SEO. </p>
+//         </section>
+//      );
+// }
  
-export default TechStack;
+// export default TechStack;
