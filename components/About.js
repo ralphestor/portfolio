942a2719a35906a98f192e16389da50063cd56c7
Ralph <ralphestor30@gmail.com>
2021-11-27 08:05:00 +0800

    added about image, removed about par

--- components/About.js
+++ components/About.js
@@ -7,18 +7,18 @@ const About = () => {
             <SectionTitle name="About Me" id="about"/>
             <div className="aboutGrid">
                 <div className="aboutGrid1">
-                    {/* <div className="wrapperDiv">
+                    <div className="wrapperDiv">
                     <Image
                         src="/RalphAbout.png"
                         alt="Ralph Estor"
                         width={300}
                         height={300}
                     />
-                    </div> */}
+                    </div>
                 </div>
                 <div className="aboutGrid2">
-                    <h4 className="aboutLbl">Overview</h4>
-                    <p className="paragraph overPar">Hi! I'm Ralph, a 22-year old web developer from Davao City, Philippines. I'm an Electronics Engineeering student who decided to code for a living because I feel the achievement everytime I create software applications (Please hire me hahaha). </p>
+                    {/* <h4 className="aboutLbl">Overview</h4>
+                    <p className="paragraph overPar">Hi! I'm Ralph, a 22-year old web developer from Davao City, Philippines. I'm an Electronics Engineeering student who decided to code for a living because I feel the achievement everytime I create software applications (Please hire me hahaha). </p> */}
                 </div>
             </div>
         </main>
