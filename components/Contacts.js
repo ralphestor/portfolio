5d8ae680ae6d66790b38818c35efdd9e67fb5add
Ralph <ralphestor30@gmail.com>
2021-11-28 21:59:31 +0800

    add no referrer

--- components/Contacts.js
+++ components/Contacts.js
@@ -12,10 +12,10 @@ const Contacts = () => {
             <div className="contactsGrid">
                 <h4>You can reach me through Gmail and Facebook</h4>
                 <div className="contactAccounts">
-                    <a href="https://mail.google.com/mail/u/0/?fs=1&tf=cm&to=ralphestor30@gmail.com&su=Your+Subject+here&body=Your+Message+here&ui=2" target="_blank">
+                    <a href="https://mail.google.com/mail/u/0/?fs=1&tf=cm&to=ralphestor30@gmail.com&su=Your+Subject+here&body=Your+Message+here&ui=2" target="_blank" rel="noreferrer">
                         <ContactLinks content={<Icon icon="simple-icons:gmail" color="#ffffff" width="90" />}/>
                     </a>
-                    <a href="https://www.facebook.com/ralphestor30" target="_blank">
+                    <a href="https://www.facebook.com/ralphestor30" target="_blank" rel="noreferrer">
                         <ContactLinks content={<Icon icon="akar-icons:facebook-fill" color="#ffffff" width="100" />}/>
                     </a>
                 </div>
@@ -23,10 +23,10 @@ const Contacts = () => {
                 <h4 className="marginTop">You can also visit my Github and LinkedIn Account</h4>
                 <div className="contactAccounts">
 
-                    <a href="https://github.com/ralphestor" target="_blank">
+                    <a href="https://github.com/ralphestor" target="_blank" rel="noreferrer">
                         <ContactLinks content={<Icon icon="akar-icons:github-fill" color="#ffffff" width="100" />}/>
                     </a>
-                    <a href="https://www.linkedin.com/in/ralphestor30/" target="_blank">
+                    <a href="https://www.linkedin.com/in/ralphestor30/" target="_blank" rel="noreferrer">
                     <ContactLinks content={<Icon icon="akar-icons:linkedin-fill" color="#ffffff" width="85" />}/>
                     </a>
                 </div>
